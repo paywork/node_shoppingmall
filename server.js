0fd8814ed36bce032bb5fe80ecb9d203e5784b58
Jean <start@paywork.io>
2020-07-31 21:07:45 +0900

    add morgan

--- server.js
+++ server.js
@@ -1,11 +1,17 @@
 
 const express = require('express')
+const morgan = require('morgan')
 const app = express()
 
 
 
 const productRoute = require('./routes/product')
 const orderRoute = require('./routes/order')
+
+
+
+// 미들웨어 설정(거쳐가는 정류장 같은 것 )
+app.use(morgan('dev'))
 
 
 app.use('/product', productRoute)
