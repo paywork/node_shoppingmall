0a267ca20b1e32fc1f53403a059d558a7650b3ab
Jean <start@paywork.io>
2020-08-14 21:15:51 +0900

    add user signup api

--- models/user.js
+++ models/user.js
@@ -3,7 +3,27 @@ const mongoose = require('mongoose')
 
 
 //2번
-const userSchema = new mongoose.Schema()
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    phone: {
+        type: Number,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
+})
+
+//이름, 이메일주소, 연락처, 패스워드
+
 
 
 //3번
--- routes/user.js
+++ routes/user.js
@@ -1,10 +1,47 @@
 //1
 const express = require('express')
 const router = express.Router()
+const userModel = require('../models/user')
+
+
+// 회원가입
+router.post('/signup', (req, res) =>{
+
+    const newUser = new userModel({
+        name: req.body.username,
+        email: req.body.email,
+        phone: req.body.phone,
+        password: req.body.password
+    })
+
+    newUser
+        .save()
+        .then(user => {
+            res.json({
+                message: "registered user",
+                userInfo: user
+            })
+        })
+        .catch(err => {
+            res.json({
+                message: err.message
+            })
+        })
 
 
 
-// User CRUD
+
+})
+
+
+
+// 로그인
+router.post('/login', (req, res) => {
+
+})
+
+
+
 
 
 
