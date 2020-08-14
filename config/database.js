d295d7ca5d391b4ff56cd913a317b01ee30fbe1a
Jean <start@paywork.io>
2020-08-14 18:44:33 +0900

    separate database from server.js to config/database.js

---
+++ config/database.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose')
+
+
+// 데이터베이스 연결
+const dbaddress = "mongodb+srv://admin:admin1234@cluster0.7l85d.mongodb.net/nodeshoppingmall?retryWrites=true&w=majority"
+// const dbaddress = "mongodb://joke716:k9915402@ds141294.mlab.com:41294/node-rest-shop"
+const dboptions = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}
+
+
+mongoose
+    .connect(dbaddress, dboptions)
+    .then(_ => console.log('mongodb connected'))
+    .catch(error => console.log(error.message))
--- server.js
+++ server.js
@@ -2,7 +2,6 @@
 const express = require('express')
 const morgan = require('morgan')
 const bodyParser = require('body-parser')
-const mongoose = require('mongoose')
 const app = express()
 
 
@@ -10,14 +9,8 @@ const app = express()
 const productRoute = require('./routes/product')
 const orderRoute = require('./routes/order')
 
-// 데이터베이스 연결
-// const dbaddress = "mongodb+srv://admin:admin1234@cluster0.7l85d.mongodb.net/nodeshoppingmall?retryWrites=true&w=majority"
-const dbaddress = "mongodb://joke716:k9915402@ds141294.mlab.com:41294/node-rest-shop"
-    ""
-mongoose
-    .connect(dbaddress, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(_ => console.log('mongodb connected'))
-    .catch(error => console.log(error.message))
+//데이터베이스 연결
+require('./config/database')
 
 
 
