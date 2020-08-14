fb2f4a45cc7e37c16ce1de4e2f8abbb52f9eba76
Jean <start@paywork.io>
2020-08-14 21:27:52 +0900

    install 'bcryptjs'

--- routes/user.js
+++ routes/user.js
@@ -1,32 +1,47 @@
 //1
 const express = require('express')
 const router = express.Router()
+const bcrypt = require('bcryptjs')
 const userModel = require('../models/user')
 
 
 // 회원가입
 router.post('/signup', (req, res) =>{
 
-    const newUser = new userModel({
-        name: req.body.username,
-        email: req.body.email,
-        phone: req.body.phone,
-        password: req.body.password
+    // password 암호화 -> DB에 저장
+    bcrypt.hash(req.body.password, 10, (err, hash) => {
+        if(err) {
+            return res.json({
+                error: err.message
+            })
+        } else {
+            // DB에 저장 
+            const newUser = new userModel({
+                name: req.body.username,
+                email: req.body.email,
+                phone: req.body.phone,
+                password: hash
+            })
+
+            newUser
+                .save()
+                .then(user => {
+                    res.json({
+                        message: "registered user",
+                        userInfo: user
+                    })
+                })
+                .catch(err => {
+                    res.json({
+                        message: err.message
+                    })
+                })
+        }
     })
 
-    newUser
-        .save()
-        .then(user => {
-            res.json({
-                message: "registered user",
-                userInfo: user
-            })
-        })
-        .catch(err => {
-            res.json({
-                message: err.message
-            })
-        })
+
+
+
 
 
 
